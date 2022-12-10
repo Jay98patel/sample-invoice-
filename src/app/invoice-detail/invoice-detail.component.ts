09c72918e2303414b3b47daef21feebf77e69820
jay98patel <itsj10.jey@gmail.com>
2022-12-10 06:18:19 +0000

    implemented methods to call the service methods

--- src/app/invoice-detail/invoice-detail.component.ts
+++ src/app/invoice-detail/invoice-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceDetails } from '../models/pageModels.interface';
+import { DbService } from '../services/db.service';
 
 @Component({
   selector: 'app-invoice-detail',
@@ -7,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceDetailComponent implements OnInit {
 
-  constructor() { }
+  invoiceList: InvoiceDetails[];
 
-  ngOnInit() {}
+  constructor(private dbService: DbService) { }
+
+  ngOnInit() {
+    this.dbService.databaseStatus().subscribe((res: boolean) => {
+      debugger
+      console.log(res)
+      // if (res) {
+        this.getInvoiceList();
+      // }
+    });
+  }
+
+  getInvoiceList() {
+    this.dbService.getInvoiceData().subscribe((res: InvoiceDetails[]) => {
+      this.invoiceList = res;
+    }, (error) => {
+      console.error('somethings went wrong');
+    });
+  }
 
 }
