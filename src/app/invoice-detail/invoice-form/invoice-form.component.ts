07da5f8cad744a448b046c694151d460a1fdc967
jay98patel <itsj10.jey@gmail.com>
2022-12-10 09:53:31 +0000

    calculation functionality

--- src/app/invoice-detail/invoice-form/invoice-form.component.ts
+++ src/app/invoice-detail/invoice-form/invoice-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-invoice-form',
@@ -9,9 +11,49 @@ export class InvoiceFormComponent implements OnInit {
 
   @Output() close = new EventEmitter<any>();
 
-  constructor() { }
+  unitNo: string[] = ['Bar', 'Piece', 'Box', 'Packet'];
 
-  ngOnInit() { }
+  invoiceForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.invoiceForm = this.buildInvoiceForm();
+    this.setTotalValue();
+  }
+
+  setTotalValue() {
+    this.invoiceForm.valueChanges.pipe(tap((res:any) => {
+      this.invoiceForm.get('total')?.setValue(res.quantity * res.price);
+      let totalPrice=this.invoiceForm.get('total')?.value;
+      if(res.discount){
+        this.invoiceForm.get('total')?.patchValue(res.quantity * res.discount - totalPrice);
+      }
+    })).subscribe()
+  }
+
+  buildInvoiceForm(): FormGroup {
+    return this.fb.group({
+      id: [null],
+      orderNo: [null],
+      eName: [null],
+      aName: [null],
+      unitNo: [null],
+      quantity: [null],
+      price: [null],
+      discount: [null],
+      total: [null],
+      taxRate1_Percentage: [null],
+      totalTax: [null],
+      taxRate1_total: [null]
+    });
+  }
+
+  saveInvoiceData() {
+    if (this.invoiceForm.valid) {
+      this.invoiceForm.value;
+    }
+  }
 
   cancel() {
     this.close.emit(null);
